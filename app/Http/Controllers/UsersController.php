069650136c415949f76772c9e8b4e86feabd6081
Pasika Volodymyr <paccika@gmail.com>
2016-09-27 18:36:18 +0300

    RA-5:default fixed

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UserCreateRequest;
+use App\Organization;
 use App\User;
 use Illuminate\Http\Request;
 use App\Http\Requests;
@@ -43,8 +44,13 @@ class UsersController extends ApiController
         ]);
 
         if($user){
-            $user->organizations()->attach(1);
+
+            #Get default organization and attach to created user
+            $default = Organization::whereDefault(1)->first();
+            $user->organizations()->attach($default->id);
+
             return Response::json(["success" => $user->toArray()], 200);
+
         }
 
     }
