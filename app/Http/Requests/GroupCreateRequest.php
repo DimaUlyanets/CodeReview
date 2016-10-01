3deb3d0686e06e002bcade0644c8b823e568b1db
Pasika Volodymyr <paccika@gmail.com>
2016-10-01 13:37:52 +0300

    RA-6 fixed

--- app/Http/Requests/GroupCreateRequest.php
+++ app/Http/Requests/GroupCreateRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Input;
 
 class GroupCreateRequest extends Request
@@ -25,10 +26,14 @@ class GroupCreateRequest extends Request
     public function rules()
     {
 
+        if(!$this->organization_id){
+            $this->organization_id = Auth::guard('api')->user()->organizations()->whereDefault(1)->first()->id;
+        }
+
         return [
             'name' => 'required|max:255|unique:groups,name,NULL,id,organization_id,' . $this->organization_id,
             'description' => 'required|max:140',
-            'organization_id' => 'numeric',
+            'organization_id' => 'numeric|exists:organizations,id',
             'privacy_id' => 'numeric|required|exists:privacy,id',
             'tags' => 'array',
         ];
