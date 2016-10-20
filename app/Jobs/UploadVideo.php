d3049dca1d5d35de14eda9be589a343601ce89d7
Dmytro Ulyanets <dulyanets@lixar.com>
2016-10-20 08:54:10 -0300

    fixed video compression

--- app/Jobs/UploadVideo.php
+++ app/Jobs/UploadVideo.php
@@ -40,14 +40,20 @@ class UploadVideo implements ShouldQueue
         #convert  video here
         #https://github.com/PHP-FFMpeg/PHP-FFMpeg
         #local vidoe stored here: storage_path('app/public').DIRECTORY_SEPARATOR .$this->local
-        $file = storage_path('app/public').DIRECTORY_SEPARATOR .$this->local;
 
-        exec('ffmpeg -i '. $file . ' -y -s 640x480 -b:v ' .env('VIDEO_BITRATE'). 'k -vcodec libx264 -acodec libmp3lame '. $file);
+        $localfile = $this->local;
+        $resized = str_replace('.mp4', 'resized.mp4', $this->local);
 
-        Storage::disk('s3')->put($this->local, file_get_contents(storage_path('app/public').DIRECTORY_SEPARATOR .$this->local), 'public');
-        $this->lesson->lesson_file = env("APP_S3") . $this->local;
+        $file = storage_path('app/public').DIRECTORY_SEPARATOR . $localfile;
+        $file2 = storage_path('app/public').DIRECTORY_SEPARATOR . $resized;
+
+        exec('ffmpeg -i '. $file . ' -y -s WxH -b:v ' .env('VIDEO_BITRATE'). 'k -vcodec libx264 -acodec libmp3lame '. $file2);
+
+        Storage::disk('s3')->put($resized, file_get_contents($file2), 'public');
+        $this->lesson->lesson_file = env("APP_S3") . $resized;
         $this->lesson->save();
-        #unlink(storage_path('app/public').DIRECTORY_SEPARATOR .$this->local);
+        unlink($file);
+        unlink($file2);
 
     }
 }
