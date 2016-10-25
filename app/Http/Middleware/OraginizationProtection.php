ab0bb8f26ba7a48e00165aa898b6589e59de6017
Markov Artur <artyr-markov@mail.ru>
2016-10-26 00:56:03 +0300

    RA-113 (add Middleware)

---
+++ app/Http/Middleware/OraginizationProtection.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+
+class OraginizationProtection
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $idOrganization = $request->route()->parameter("id");
+        $user = Auth::guard('api')->user();
+        $userRole = DB::table('organization_user')->select('role')->where('user_id', '=', $user->id)->where('organization_id', '=', $idOrganization)->get();
+        if($userRole->toArray()[0]->role=="admin"){
+            return $next($request);
+        }
+        return response()->json(['error' => 'no access rights'], 403);
+    }
+}
