709ec545cb3b912bda8913a232cc00ab94b08a3c
Markov Artur <artyr-markov@mail.ru>
2016-11-01 23:52:41 +0200

    fix bug with Ids

--- app/Http/Middleware/OrganizationProtection.php
+++ app/Http/Middleware/OrganizationProtection.php
@@ -20,8 +20,10 @@ class OrganizationProtection
         $idOrganization = $request->route()->parameter("id");
         $user = Auth::guard('api')->user();
         $userRole = DB::table('organization_user')->select('role')->where('user_id', '=', $user->id)->where('organization_id', '=', $idOrganization)->get();
-        if($userRole->toArray()[0]->role=="admin"||$userRole->toArray()[0]->role=="owner"){
-            return $next($request);
+        if(count($userRole)>0) {
+            if ($userRole->toArray()[0]->role == "admin" || $userRole->toArray()[0]->role == "owner") {
+                return $next($request);
+            }
         }
         return response()->json(['error' => 'no access rights'], 403);
     }
