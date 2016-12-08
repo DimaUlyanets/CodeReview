20cd0fd30458acc62f021a656a6824dcef97d1b4
Dmytro Ulyanets <dulyanets@lixar.com>
2016-12-08 15:19:04 -0400

    added tags to search

---
+++ app/Listeners/EventTopicAddToIndex.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Listeners;
+
+use App\ElasticSearch\TopicSearch;
+use App\Events\ElasticTopicAddToIndex;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class EventClassAddToIndex
+{
+    public function __construct(){
+
+    }
+    public function handle(ElasticTopicAddToIndex $event){
+        $search = new TopicSearch();
+        $search->addToIndex($event->id, $event->name, $event->followers);
+    }
+}
